7d83fc7dedeea184772b12c348df6ad230f6c835
baltazarparra <baltazarparra@outlook.com>
2019-11-08 08:48:40 -0300

    Add scroll control

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Tween, Timeline } from 'react-gsap'
-// import { Controller, Scene } from 'react-scrollmagic'
+import { Controller, Scene } from 'react-scrollmagic'
 
 const Wrapper = styled.div`
   position: relative;
-  height: 80vh;
-  width: auto;
   margin: 40px 16px 0;
   padding: 10px;
   overflow: hidden;
@@ -17,22 +15,34 @@ function App() {
   return (
     <div className="App">
       <Wrapper>
-        <Timeline
-          target={
-            <div>Hell Yah</div>
-          }
-        >
-          <Tween from={{ x: '100px', rotation: -360 }} />
-          <Tween to={{ x: '50px', rotation: -90 }} />
-          <Timeline
-            target={
-              <div>Fuc ie</div>
-            }
-          >
-            <Tween from={{ x: '200px', rotation: -60 }} />
-            <Tween to={{ x: '70px', rotation: -40 }} />
-          </Timeline>
-        </Timeline>
+        <Controller>
+          <Scene triggerHook="onLeave" duration={3500} pin>
+            <Timeline
+              target={
+                <div>Hell Yah</div>
+              }
+            >
+              <Tween from={{ x: '100px', rotation: -360 }} />
+              <Tween to={{ x: '50px', rotation: -90 }} />
+              <Timeline
+                target={
+                  <div>Fuc ie</div>
+                }
+              >
+                <Tween from={{ x: '200px', rotation: -60 }} />
+                <Tween to={{ x: '70px', rotation: -40 }} />
+                <Timeline
+                  target={
+                    <div>Holly Molly</div>
+                  }
+                >
+                  <Tween from={{ x: '140px', rotation: -100 }} />
+                  <Tween to={{ x: '20px', rotation: -140 }} />
+                </Timeline>
+              </Timeline>
+            </Timeline>
+          </Scene>
+        </Controller>
       </Wrapper>
     </div>
   )
