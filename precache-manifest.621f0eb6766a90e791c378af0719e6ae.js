f1a36baf29257e42137bb1ea20075a302009585b
baltazarparra <baltazarparra@outlook.com>
2019-11-08 10:21:36 -0300

    Updates

---
+++ precache-manifest.621f0eb6766a90e791c378af0719e6ae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66269978e88ccc71084be3ea862d7034",
+    "url": "/scroll-animation/index.html"
+  },
+  {
+    "revision": "ae8dfbb333299f1510da",
+    "url": "/scroll-animation/static/js/2.6cd65446.chunk.js"
+  },
+  {
+    "revision": "25b55bbad5880c15de3b",
+    "url": "/scroll-animation/static/js/main.5bedfcfa.chunk.js"
+  },
+  {
+    "revision": "b71e6df8d3cbf468be25",
+    "url": "/scroll-animation/static/js/runtime-main.eaf9de55.js"
+  }
+]);
