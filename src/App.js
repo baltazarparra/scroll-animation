a31310c1b2232705b8c8f839b398583edc4473af
baltazarparra <baltazarparra@outlook.com>
2019-11-08 09:24:38 -0300

    Finish motion

--- src/App.js
+++ src/App.js
@@ -5,48 +5,69 @@ import { Controller, Scene } from 'react-scrollmagic'
 
 const Wrapper = styled.div`
   position: relative;
-  margin: 40px 16px 0;
-  padding: 10px;
+  background-color: #222;
+  background-image: url(https://pngriver.com/wp-content/uploads/2018/04/Download-Raven-PNG.png);
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-position: top;
+
+  font-family: monospace;
+  font-size: 1.6em;
   overflow: hidden;
-  background-color: silver;
 `
 
 function App() {
   return (
-    <div className="App">
-      <Wrapper>
-        <Controller>
-          <Scene triggerHook="onLeave" duration={7000} pin>
+    <Wrapper>
+      <Controller>
+        <Scene duration={3200} pin>
+          <Timeline
+            target={
+              <div
+                style={{
+                  background: '#f00'
+                }}
+              >
+                Quoth the
+              </div>
+            }
+          >
+            <Tween duration={1} ease="Power3.easeOut" from={{ x: '100px' }} />
+            <Tween to={{ x: '50px' }} />
             <Timeline
+              delay={-2}
               target={
-                <div>Hell Yah</div>
+                <div
+                  style={{
+                    background: '#f99'
+                  }}
+                >
+                  Raven:
+                </div>
               }
             >
-              <Tween ease="Power3.easeOut" from={{ x: '100px', rotation: -360 }} />
-              <Tween to={{ x: '50px', rotation: -90 }} />
+              <Tween duration={1} ease="Power3.easeOut" from={{ x: '200px', y: '-10px' }} />
+              <Tween to={{ x: '70px', y: '5px' }} />
               <Timeline
-                delay={-2}
+                delay={-1}
                 target={
-                  <div>Fuc ie</div>
+                  <h1
+                    style={{
+                      background: '#f55'
+                    }}
+                  >
+                    Nevermoar!
+                  </h1>
                 }
               >
-                <Tween ease="Power3.easeOut" from={{ x: '200px', rotation: -60 }} />
-                <Tween to={{ x: '70px', rotation: -40 }} />
-                <Timeline
-                  delay={-1}
-                  target={
-                    <div>Holly Molly</div>
-                  }
-                >
-                  <Tween ease="Power3.easeOut" from={{ x: '140px', rotation: -100 }} />
-                  <Tween to={{ x: '20px', rotation: -140 }} />
-                </Timeline>
+                <Tween duration={2} ease="Power3.easeOut" from={{ x: '140px', y: '60px' }} />
+                <Tween to={{ x: '20px', y: '30px' }} />
               </Timeline>
             </Timeline>
-          </Scene>
-        </Controller>
-      </Wrapper>
-    </div>
+          </Timeline>
+        </Scene>
+      </Controller>
+    </Wrapper>
   )
 }
 
